73b2d4648e9aac573d131bc8df5b51db0b6cc8f6
Program better, faster, stronger <cmw2@sting.graceland.edu>
2018-03-30 12:47:02 -0500

    Addition of JavaScript Practice

---
+++ info.js
@@ -0,0 +1,8 @@
+var name = "Chase Wood";
+var date = "Mar 30 2018";
+
+document.write("<span id ='footer'>Jacob's Website</span><br/>");
+document.write("&#169 2018 " + name);
+document.write("<br/>Modified " + date);
+document.write("<br/>Retrieved on " + Date());
+
